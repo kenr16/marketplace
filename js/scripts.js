8c59959798c1aa14a5b888a1555c6e7e47dc212f
Ken Rutan and Nick Powell <ken@>
2017-03-22 14:43:22 -0700

    add initial html and js. add radio input buttons and other forms.

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    var name1Input = $("input#name").val();
+    var person2Input = $("input#lastName").val();
+    var animalInput = $("input#address1").val();
+    var exclamationInput = $("input#address2").val();
+    var verbInput = $("input#city").val();
+
+    var stateInput = $("#stateSelect").val();
+
+    var flavorInput = $("input:radio[name=flavor]:checked").val();
+
+    $(".name").text(name1Input);
+    $(".lastName").text(person2Input);
+    $(".address1").text(animalInput);
+    $(".address2").text(exclamationInput);
+    $(".city").text(verbInput);
+
+    $(".state").text(stateInput);
+    $(".flavor").text(flavorInput);
+
+
+    $("#story").show();
+
+    event.preventDefault();
+  });
+});
